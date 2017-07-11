ad0cbcc4b7d4bca309825d89ebf2d63f17352f36
Jobby00 <jobby00@gmail.com>
2017-07-11 15:04:18 +0200

    suppresion des images et aussi des petite modifications

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -7,40 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 use MicroCMS\Domain\Article;
 
 class ApiController {
-
-    /**
-     * API articles controller.
-     *
-     * @param Application $app Silex application
-     *
-     * @return All articles in JSON format
-     */
-    public function getArticlesAction(Application $app) {
-        $articles = $app['dao.article']->findAll();
-        // Convert an array of objects ($articles) into an array of associative arrays ($responseData)
-        $responseData = array();
-        foreach ($articles as $article) {
-            $responseData[] = $this->buildArticleArray($article);
-        }
-        // Create and return a JSON response
-        return $app->json($responseData);
-    }
-
-    /**
-     * API article details controller.
-     *
-     * @param integer $id Article id
-     * @param Application $app Silex application
-     *
-     * @return Article details in JSON format
-     */
-    public function getArticleAction($id, Application $app) {
-        $article = $app['dao.article']->find($id);
-        $responseData = $this->buildArticleArray($article);
-        // Create and return a JSON response
-        return $app->json($responseData);
-    }
-
     /**
      * API create article controller.
      *
